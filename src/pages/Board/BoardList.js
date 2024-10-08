2ccd482b2cd5cfd2d69283ec33215e3fcacddaf5
xoone9 <kksw1130@naver.com>
2024-10-09 06:43:40 +0900

    20241029/0624

--- src/pages/Board/BoardList.js
+++ src/pages/Board/BoardList.js
@@ -4,7 +4,122 @@ function BoardList (){
 
     return(
 
-        <p>보드리스트</p>
+
+        <div className='container'>
+            {/* #게시글 검색  */}
+            <form></form> 
+
+
+            <div className='table'>
+                <table className='dma'>
+                    <thead>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>
+                        <tr>
+                            <td>번호</td>
+                            <td>위치</td>
+                            <td>음식종류</td>
+                            <td>제목</td>
+                            <td>작성자</td>
+                            <td>작성일</td>
+                            <td>추천수</td>                            
+                        </tr>                        
+                    </tbody>
+
+
+
+
+                </table>
+
+
+            </div>
+
+
+
+
+
+        </div>      
 
     )
 
